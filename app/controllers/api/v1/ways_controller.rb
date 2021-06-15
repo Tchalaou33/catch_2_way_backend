587abf5bbf77818a0f5cb2ef70d4ab1cd01b518c
Tchalaou33 <attchalaou@gmail.com>
2021-06-15 05:11:52 -0400

    added relevent routes to way controller and added create route

--- app/controllers/api/v1/ways_controller.rb
+++ app/controllers/api/v1/ways_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::WaysController < ApplicationController
+
+    def index
+        ways = Way.all
+        render json: ways
+    end
+
+    def create
+        way = Ways.new(way_params)
+        if way.save
+            render json: way, status: :accepted
+        else
+            render json: {errors: way.errors.full_messages}, Status: :unprocessible_entity
+
+        end
+    end
+
+    private
+    
+    def way_params
+        params.require(:way).permit(:color, :description, :destination_id)
+    end
+
+
 end
