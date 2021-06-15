2f5def4ac61b5c7eeca8da0c2d184f1b0ccd7326
Tchalaou33 <attchalaou@gmail.com>
2021-06-15 13:11:46 -0400

    destination controller index and create routes

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::DestinationsController < ApplicationController
+
+    def index
+        destinations = Destination.all
+        render json: destinations
+    end
+
+    def create 
+        destination.new(destination_params)
+        if destination.save
+            render json: destination, status: :accepted
+        else
+            render json: {errors: destination.errors.full_messages}, status: :unproccessible_entity
+        end
+    end
+
+    private
+    
+    def destination_params
+        params.require(:destination).permit(:name)
+    end
 end
